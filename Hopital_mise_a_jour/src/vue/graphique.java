561391d4f749e20e72667431c5e00919e50c24d8
tombrusselle <brusselletom@gmail.com>
2018-04-21 17:03:51 +0200

    mise a jour effectué, seul probleme quand insertion

--- Hopital_mise_a_jour/src/vue/graphique.java
+++ Hopital_mise_a_jour/src/vue/graphique.java
@@ -8,6 +8,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 
@@ -23,18 +26,10 @@ public class graphique extends JFrame implements ActionListener {
     JButton boutonco = new JButton();
     JButton boutonrech = new JButton();
     JButton boutonmaj = new JButton();
-    JButton boutonrepor = new JButton();
-    
+    JButton boutonrepor = new JButton();  
     private JButton imag = new JButton("h");
     
-    
-    //jfarme touchant a la mise a jour
-    
-    JPanel panel_MAJ = new JPanel();
-    JButton retour = new JButton();
-    JButton modifier  = new JButton();
-    JButton supprimer = new JButton();
-    JButton inserer = new JButton();
+   
         
     // contructeur
     public graphique () {
@@ -44,6 +39,7 @@ public class graphique extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         
         pan = new JPanel();     // instancier le panneau
         getContentPane().add(pan, BorderLayout.NORTH);
@@ -54,15 +50,6 @@ public class graphique extends JFrame implements ActionListener {
         pan3 = new JPanel();     // instancier le panneau
         getContentPane().add(pan3, BorderLayout.SOUTH);
         
-        /*JPanel connexion = new JPanel();
-        connexion.setBackground(Color.red);
-        
-        JPanel maj = new JPanel();
-        maj.setBackground(Color.blue);
-        
-        JPanel reporting = new JPanel();
-        reporting.setBackground(Color.green);
-        */
         
         boutonco = new JButton("Connexion");
         boutonco.setPreferredSize(new Dimension(200,80));
@@ -91,25 +78,20 @@ public class graphique extends JFrame implements ActionListener {
         JButton boutonequipe = new JButton("Gestion d'un centre hospitalier par Vincent B, Tom B et Delphine H");
         boutonequipe.setPreferredSize(new Dimension(600,50));
         pan3.add(boutonequipe);
+
         
-        
-        //le panneau mise a jour est invisible 
-        //panel_MAJ.setVisible(false);
-        
+
         setVisible(true);
                 
     }
     
     
     
-    public void MiseAJour()
-    {
-        ZDialog MAJ = new ZDialog(null,"Mise A Jour",true);
-        panel_MAJ.setVisible(true);
+
 
         
         
-    }
+    
     
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -128,25 +110,11 @@ public class graphique extends JFrame implements ActionListener {
                 //mise a jour
                 if (source == boutonmaj) 
                 {
-                    MiseAJour();
+                    //on fait un try catch afin de gerer les erreurs sqlexeption et
+                    MiseAJour objetmaj = new MiseAJour();
+                    objetmaj.FX_MiseAJour();
                 }
-                //sous boutons de mise a jour
-                if (source == retour) 
-                {
-                    
-                }
-                if (source == modifier) 
-                {
-                    
-                }
-                if (source == supprimer) 
-                {
-                    
-                }
-                if (source == inserer) 
-                {
-                    
-                }
+
                 
                 
                 
